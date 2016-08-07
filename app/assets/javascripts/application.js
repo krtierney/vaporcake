d8950c0eb215c6a9ba029bd1c2495920c4f8caf4
Kaitlyn Tierney <k@krtierney.com>
2016-08-07 14:44:37 +0100

    broke the js textarea fix into a separate file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-$(function () {
-    $("textarea").each(function () {
-        this.style.height = (this.scrollHeight)+'px';
-    });
-});
-
-function autoresize(textarea) {
-    textarea.style.height = '0px';     //Reset height, so that it not only grows but also shrinks
-    textarea.style.height = (textarea.scrollHeight) + 'px';    //Set new height
-}
-
-$('.autosize').keyup(function () {
-    autoresize(this);
-});
