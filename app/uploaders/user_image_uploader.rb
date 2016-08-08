6a43b8df15d8a6e75c1c047eadfad8f1e4c7c141
Kaitlyn Tierney <k@krtierney.com>
2016-08-08 16:50:02 +0100

    restricted uploaders to images

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -41,10 +41,10 @@ class UserImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
+  def content_type_whitelist
+    /image\//
+  end
+  
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
