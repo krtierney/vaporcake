3c44e2b8b5987fccf0035dfa7354b3a915c4fd66
Kaitlyn Tierney <k@krtierney.com>
2016-08-10 15:04:51 +0100

    Changing aws bucket region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-      region: 'eu-west-1'
+      region: 'us-west-1'
     }
 
     config.fog_directory = ENV['AWS_BUCKET_NAME']
